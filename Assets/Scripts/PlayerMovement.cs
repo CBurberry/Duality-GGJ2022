cb8c0ce297d32ca03502f2044cededa55863b434
Sam Burton <samburton93@gmail.com>
2022-01-29 15:00:37 +0000

    PlayerMovementController

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,82 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public static PlayerMovement instance;
 
-    [SerializeField] private const float SPEED = 50f;
+    [SerializeField] private const float SPEED = 5f;
 
-    [SerializeField] private GameObject playerBase;
+    private GameObject playerBase;
     [SerializeField] private State state;
-    [SerializeField] private Rigidbody2D playerRigidbody2D;
-    [SerializeField] private Vector3 moveDir;
+    private Rigidbody2D playerRigidbody2D;
+    private InputActions inputActions;
 
     /////// STATE OPTIONS ///////
     private enum State 
     {
         Normal,
     }
-
+    
     /////// STATE INITIALIZATION ///////
     private void Awake() 
     {
         instance = this;
         playerBase = gameObject.GetComponent<GameObject>();
         playerRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-        SetStateNormal();
+        state = State.Normal;   //set state
+
+        inputActions = new InputActions();
     }
 
+    /////// TURN ON INPUT ///////
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    } 
+
+    /////// PER FRAME ACTIONS ///////
     private void Update() 
     {
-        switch (state) {
+        switch (state) 
+        {
         case State.Normal:
-            HandleMovement();
+            Vector2 move = inputActions.Player.Move.ReadValue<Vector2>();
+            Debug.Log(move);
+
+            bool isIdle = inputActions.Player.Move.ReadValue<Vector2>() == Vector2.zero;
+
+            if (isIdle) 
+            {
+                //idle animation
+            } 
+
+            else 
+            {
+                //move animation
+                
+                playerRigidbody2D.MovePosition(transform.position + (Vector3)move * SPEED * Time.fixedDeltaTime);
+            }
             break;
         }
-    }
-    
-    /////// STATE INITIALIZATION ///////
-    private void SetStateNormal() 
-    {
-        state = State.Normal;
-    }
-
-    /////// TRANSLATE INPUT TO VECTOR ///////
-    /////// NEEDS CHANGE TO NEW INPUT SYSTEM ////////
-    private void HandleMovement() 
-    {
-        float moveX = 0f;
-        float moveY = 0f;
-        
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            moveY = +1f;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            moveY = -1f;
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            moveX = -1f;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            moveX = +1f;
-        }
-        
-        moveDir = new Vector3(moveX, moveY).normalized;
-    }
-
-    /////// TRANSLATE INPUT TO VECTOR ///////
-    private void FixedUpdate() 
-    {
-        bool isIdle = moveDir.x == 0 && moveDir.y == 0;
-        if (isIdle) {
-            //playerBase.PlayIdleAnim();
-        } else {
-            //playerBase.PlayMoveAnim(moveDir);
-            //transform.position += moveDir * SPEED * Time.deltaTime;
-            playerRigidbody2D.MovePosition(transform.position + moveDir * SPEED * Time.fixedDeltaTime);
-        }
-        
     }
 }
