33b69dc8c73d6ab629fac367e69686bb94d583cc
Christian <christianburberry@gmail.com>
2022-01-30 11:42:46 +0000

    Removing flag states as no longer needed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Singleton class for in-between scenes data persistence and scene transition handler.
+/// Singleton class for scene transition handler and dependency constructor.
 /// Add and set flags for individual elements in the scene.
 /// </summary>
 public class GameManager : MonoBehaviour
@@ -18,35 +18,6 @@ public class GameManager : MonoBehaviour
             return instance;
         }
     }
-
-    #region AreaFlags
-    public enum AreaOneFlags
-    {
-        Default = 0,
-        //NewFlag = 1 << 0,
-        //NewFlag = 1 << 1,
-        //NewFlag = 1 << 2
-    }
-
-    public enum AreaTwoFlags
-    {
-        Default = 0,
-    }
-
-    public enum AreaThreeFlags
-    {
-        Default = 0,
-    }
-
-    [EnumFlags]
-    public AreaOneFlags areaOneFlags;
-
-    [EnumFlags]
-    public AreaTwoFlags areaTwoFlags;
-
-    [EnumFlags]
-    public AreaThreeFlags areaThreeFlags;
-    #endregion
 
     void Awake()
     {
